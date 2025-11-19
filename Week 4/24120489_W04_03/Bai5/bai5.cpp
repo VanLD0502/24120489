558b0c57f014d2591c2a53ff09f9d7e3ccac5bac
Van <vanpc1906@gmail.com>
2025-11-19 11:00:00 +0700

    OK

--- Week 4/24120489_W04_03/Bai5/bai5.cpp
+++ Week 4/24120489_W04_03/Bai5/bai5.cpp
@@ -242,25 +242,29 @@ ostream &operator<<(ostream &os, const SoNguyenLon &other)
 }
 int main()
 {
-    SoNguyenLon snl1;
-    SoNguyenLon snl2(44667733);
-    SoNguyenLon snl3(5, 9);
-    SoNguyenLon snl4(7, 30);
-    SoNguyenLon snl5 = snl3 - snl2;
-    SoNguyenLon snl6 = 1098765432 - snl2;
-    SoNguyenLon snl7 = snl4 - snl3 + 123456789;
-    SoNguyenLon snl8 = snl2 * snl3;
-    cout << snl1 << endl
-         << snl2 << endl
-         << snl3 << endl;
-    cout << snl4 << endl
-         << snl5 << endl
-         << snl6 << endl;
-    cout << snl7 << endl
-         << snl8 << endl;
+    // SoNguyenLon snl1;
+    // SoNguyenLon snl2(44667733);
+    // SoNguyenLon snl3(5, 9);
+    // SoNguyenLon snl4(7, 30);
+    // SoNguyenLon snl5 = snl3 - snl2;
+    // SoNguyenLon snl6 = 1098765432 - snl2;
+    // SoNguyenLon snl7 = snl4 - snl3 + 123456789;
+    // SoNguyenLon snl8 = snl2 * snl3;
+    // cout << snl1 << endl
+    //      << snl2 << endl
+    //      << snl3 << endl;
+    // cout << snl4 << endl
+    //      << snl5 << endl
+    //      << snl6 << endl;
+    // cout << snl7 << endl
+    //      << snl8 << endl;
 
+    char *a = new char[100];
+    const char *b = "Hello";
+    strcpy_s(a, 100, b);
+    cout << a << endl;
     system("pause");
     return 0;
 }
 
-//24815407197406815
+// 24815407197406815
